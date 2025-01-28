c59532c4bbf6c961dd7805df61092bd432d1d056
Kotrel Manurung <kotrel@MacBook-Pro-Kotrel.local>
2025-01-29 01:20:53 +0700

    feat: add product logic (search, category, order, and select all

--- src/components/card-product.tsx
+++ src/components/card-product.tsx
@@ -1,54 +1,91 @@
-import { Box, HStack, Image, Text, VStack, Button } from '@chakra-ui/react';
-import { productDummy } from './product-dummy';
+import {
+  Box,
+  HStack,
+  Input,
+  Image,
+  Text,
+  VStack,
+  Button,
+} from '@chakra-ui/react';
+import { Checkbox } from '../components/ui/checkbox';
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import React, { useState } from 'react';
+import { IoIosLink } from 'react-icons/io';
+import { Switch } from '@/components/ui/switch';
+import { Field } from '@/components/ui/field';
 
-type Status =
-  | 'Belum Dibayar'
-  | 'Aktif'
-  | 'Non-Aktif'
-  | 'Dalam Pengiriman'
-  | 'Pesanan Selesai'
-  | 'Dibatalkan';
-
-const statusColors: Record<Status, string> = {
-  'Belum Dibayar': 'yellow.400',
-  Aktif: 'green.600',
-  'Non-Aktif': 'gray.300',
-  'Dalam Pengiriman': 'orange.400',
-  'Pesanan Selesai': 'blue.500',
-  Dibatalkan: 'red.500',
-};
-
-const textColor: Record<Status, string> = {
-  'Belum Dibayar': 'black',
-  Aktif: 'white',
-  'Non-Aktif': 'white',
-  'Dalam Pengiriman': 'white',
-  'Pesanan Selesai': 'black',
-  Dibatalkan: 'white',
-};
-
-// const buttonText: Record<Status, string> = {
-//   'Belum Dibayar': 'Hubungi Pembeli',
-//   'Aktif': 'Proses Pesanan',
-//   'Non-Aktif': 'Kabari Pembeli',
-//   'Dalam Pengiriman': 'Lihat Rincian Pengiriman',
-//   'Pesanan Selesai': 'Hubungi Pembeli',
-//   Dibatalkan: 'white',
-// };
-
-interface CardProductProps {
-  statusFilter: Status | 'semua';
+interface Product {
+  id: number;
+  status: string;
+  kode: string;
+  product: {
+    nama: string;
+    jumlah: number;
+    category: string;
+    harga: number;
+    imageUrl: string;
+  };
 }
 
-export default function CardProduct({ statusFilter }: CardProductProps) {
-  const filteredProduct =
-    statusFilter === 'semua'
-      ? productDummy
-      : productDummy.filter((product) => product.status === statusFilter);
+interface CardProductProps {
+  products: Product[]; // menerima array produk
+}
+
+export default function CardProduct({ products }: CardProductProps) {
+  const statusColors: Record<string, string> = {
+    'Belum Dibayar': 'yellow.400',
+    Aktif: 'green.600',
+    'Non-Aktif': 'gray.300',
+    'Dalam Pengiriman': 'orange.400',
+    'Pesanan Selesai': 'blue.500',
+    Dibatalkan: 'red.500',
+  };
+
+  const textColor: Record<string, string> = {
+    'Belum Dibayar': 'black',
+    Aktif: 'white',
+    'Non-Aktif': 'white',
+    'Dalam Pengiriman': 'white',
+    'Pesanan Selesai': 'black',
+    Dibatalkan: 'white',
+  };
+
+  const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
+  const handleSelectAll = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setSelectedProducts(products.map((product) => product.id));
+    } else {
+      setSelectedProducts([]);
+    }
+  };
+
+  const handleProductSelect = (productId: number) => {
+    if (selectedProducts.includes(productId)) {
+      setSelectedProducts(selectedProducts.filter((id) => id !== productId));
+    } else {
+      setSelectedProducts([...selectedProducts, productId]);
+    }
+  };
 
   return (
     <>
-      {filteredProduct.map((product) => (
+      <Checkbox
+        isChecked={selectedProducts.length === products.length}
+        onChange={handleSelectAll}
+        mb={4}
+      >
+        Pilih Semua
+      </Checkbox>
+      {products.map((product) => (
         <Box
           key={product.id}
           borderWidth={'1px'}
@@ -56,33 +93,26 @@ export default function CardProduct({ statusFilter }: CardProductProps) {
           borderRadius={'md'}
           mb={4}
         >
+          <Checkbox
+            checked={selectedProducts.includes(product.id)}
+            onChange={() => handleProductSelect(product.id)}
+            mb={2}
+          ></Checkbox>
           <HStack justifyContent={'space-between'} px={3} pt={2}>
             <Box
               width={'auto'}
               px={2}
               borderRadius={'md'}
-              bg={statusColors[product.status as Status] || 'gray.400'}
+              bg={statusColors[product.status] || 'gray.400'}
             >
               <Text
                 textAlign={'center'}
                 fontSize={'14px'}
-
-                color={textColor[product.status as Status] || 'black'}
+                color={textColor[product.status] || 'black'}
               >
                 {product.status}
               </Text>
             </Box>
-            {/* <Button
-              height={'30px'}
-              borderRadius={'full'}
-              color={'black'}
-              bg={'white'}
-              borderWidth={'1px'}
-              borderColor={'gray.300'}
-              fontSize={'14px'}
-            >
-              {buttonText[produk.status as Status] || 'Hubungi Pembeli'}
-            </Button> */}
           </HStack>
           <Box borderTopWidth={'1px'} borderColor={'gray.200'}>
             <HStack alignItems={'center'} p={3}>
@@ -115,15 +145,57 @@ export default function CardProduct({ statusFilter }: CardProductProps) {
                 <Text fontWeight={'medium'} fontSize={'14px'}>
                   Rp{product.product.harga.toLocaleString()}
                 </Text>
+                <Switch colorPalette={'cyan'}></Switch>
               </VStack>
             </HStack>
+            <DialogRoot>
+              <DialogTrigger asChild>
+                <Button size="xs" colorScheme="blue" variant="outline">
+                  Ubah Harga
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Ubah Harga</DialogTitle>
+                </DialogHeader>
+                <DialogBody pb="4">
+                  <Field label="Ubah harga untuk produk TAS RANSEL WANITA">
+                    <Input placeholder="Harga*" />
+                  </Field>
+                </DialogBody>
+                <DialogFooter>
+                  <DialogActionTrigger asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DialogActionTrigger>
+                  <Button>Save</Button>
+                </DialogFooter>
+              </DialogContent>
+            </DialogRoot>
+            <DialogRoot>
+              <DialogTrigger asChild>
+                <Button size="xs" colorScheme="blue" variant="outline">
+                  Ubah Stok
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Ubah Stok </DialogTitle>
+                </DialogHeader>
+                <DialogBody pb="4">
+                  <Field label="Ubah stok untuk produk TAS RANSEL WANITA">
+                    <Input placeholder="First Name" />
+                  </Field>
+                </DialogBody>
+                <DialogFooter>
+                  <DialogActionTrigger asChild>
+                    <Button variant="outline">Batalkan</Button>
+                  </DialogActionTrigger>
+                  <Button>Simpan</Button>
+                </DialogFooter>
+              </DialogContent>
+            </DialogRoot>
             <Button size="xs" colorScheme="blue" variant="outline">
-              Ubah Harga
-            </Button>
-            <Button size="xs" colorScheme="blue" variant="outline">
-              Ubah Stok
-            </Button>
-            <Button size="xs" colorScheme="blue" variant="outline">
+              <IoIosLink />
               Lihat Halaman
             </Button>
           </Box>
